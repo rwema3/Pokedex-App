c8199f94f258b430f439e60f20399718aaf81cca
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-23 19:14:04 +0200

    Update pokemon_info.dart

--- pokemon_info.dart
+++ pokemon_info.dart
@@ -2,3 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart' hide AnimatedSlide;
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pokedex/configs/colors.dart';
+import 'package:pokedex/configs/images.dart';
+import 'package:pokedex/domain/entities/pokemon.dart';
+import 'package:pokedex/domain/entities/pokemon_props.dart';
