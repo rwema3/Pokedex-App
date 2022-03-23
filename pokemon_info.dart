813799ad7c30bb871aed56cb40edcfee434358ce
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-23 19:44:28 +0200

    Update pokemon_info.dart

--- pokemon_info.dart
+++ pokemon_info.dart
@@ -29,3 +29,4 @@ part 'sections/pokemon_info_card_about.dart';
 part 'sections/pokemon_info_card_basestats.dart';
 part 'sections/pokemon_info_card_evolutions.dart';
 
+class PokemonInfo extends StatefulWidget {
