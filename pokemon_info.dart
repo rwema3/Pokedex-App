8d7ed1fecbc4366b12c69d375de49c1a1e6b1b73
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-23 18:22:40 +0200

    Update pokemon_info.dart

--- pokemon_info.dart
+++ pokemon_info.dart
@@ -26,4 +26,4 @@ part 'sections/background_decoration.dart';
 part 'sections/pokemon_overall_info.dart';
 part 'sections/pokemon_info_card.dart';
 part 'sections/pokemon_info_card_about.dart';
-part 'sections/pokemon_info_card_basesta
+part 'sections/pokemon_info_card_basest
