afa7903b3fee776152225f5e2cf4501cedfc1ef7
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-23 18:14:27 +0200

    Update pokemon_info.dart

--- pokemon_info.dart
+++ pokemon_info.dart
@@ -48,4 +48,3 @@ class _PokemonInfoState extends State<PokemonInfo> with TickerProviderStateMixin
     _rotateController = AnimationController(
       vsync: this,
       duration: Duration(milliseconds: 5000),
-    )..repeat();
